ad0ed30aa915f8edcd4b593fcaa15f1ddfd4b1bc
Adneths <bill.lui0921@gmail.com>
2023-04-18 18:46:18 -0700

    Test lib

--- src/core/lib.cpp
+++ src/core/lib.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string>
 
+#include "core/Player.h"
+
 void test_core(const std::string &source) {
   std::cout << "hello " + source + ", from core!" << std::endl;
+  Player* p = new Player();
+  delete p;
 }
