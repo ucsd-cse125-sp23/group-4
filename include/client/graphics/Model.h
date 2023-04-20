907431210e7f093c66302d1ce416a29e3411ff5d
Eddie Lin <linhancheng14@gmail.com>
2023-04-20 00:29:02 -0700

    Add initial graphics code

---
+++ include/client/graphics/Model.h
@@ -0,0 +1,36 @@
+/**************************************************
+A model is a mesh with material (and more?? animation skeleton???)
+*****************************************************/
+#include "Mesh.h"
+#include "Material.h"
+
+#ifndef __MODEL_H__
+#define __MODEL_H__
+
+struct Model {
+    glm::mat4 transformMtx; // applied first!
+
+    Mesh* mesh;
+    Material* material;
+    // TODO, consider adding skin stuff here
+
+    void draw(const glm::mat4& viewProjMtx, const glm::mat4& modelMtx)
+    {
+        if (!material || !mesh)
+            return;
+
+        GLuint shader = material->shader;
+
+        // actiavte the shader program      ---
+        glUseProgram(shader);
+
+        material->setUniforms(viewProjMtx, modelMtx * transformMtx);
+
+        mesh->draw();
+
+        // deactivate the shader program    ---
+        glUseProgram(0);
+    }
+};
+
+#endif 
