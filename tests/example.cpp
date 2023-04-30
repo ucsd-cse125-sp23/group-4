a60b2d488b9cce648db3a276332d30e15caaadbb
Sean Ye <seanye24@gmail.com>
2023-04-29 19:16:31 -0700

    Disable example fail test

--- tests/example.cpp
+++ tests/example.cpp
@@ -2,4 +2,4 @@
 
 TEST_CASE("Example passing test", "[example]") { REQUIRE(1 + 1 == 2); }
 
-TEST_CASE("Example failing test", "[example]") { REQUIRE(1 + 1 == 3); }
+// TEST_CASE("Example failing test", "[example]") { REQUIRE(1 + 1 == 3); }
