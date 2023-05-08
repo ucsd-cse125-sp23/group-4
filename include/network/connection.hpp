70eb1dbf61a6cfcc078066ffe9c851b47593fa2f
Sean Ye <seanye24@gmail.com>
2023-05-08 14:54:16 -0700

    Refactor connection handling
    * Refactor parsing into method
    * Make data reading synchronous

--- include/network/connection.hpp
+++ include/network/connection.hpp
@@ -10,6 +10,8 @@
 #include <utility>
 #include <vector>
 
+#include "boost/system/detail/error_code.hpp"
+
 using boost::asio::ip::tcp;
 
 template <typename T>
@@ -28,6 +30,11 @@ class Connection : std::enable_shared_from_this<Connection<T>> {
   void write(const T&);
 
  private:
+  std::size_t parse_header();
+  T parse_data();
+  std::string build_header();
+  std::string serialize_data(const T&);
+
   tcp::socket socket_;
   const int header_length_ = 8;
   char inbound_header_[8];
@@ -44,79 +51,89 @@ void Connection<T>::start() {
 }
 
 template <typename T>
-void Connection<T>::read() {
-  // Issue a read operation to read exactly the number of bytes in a header.
-  boost::asio::async_read(
-      socket_, boost::asio::buffer(inbound_header_),
-      [&](boost::system::error_code ec, std::size_t length) {
-        if (ec) {
-          read_handler(ec, T());
-          return;
-        }
-        std::cout << "Received message" << std::endl;
-
-        std::istringstream is(std::string(inbound_header_, header_length_));
-        std::size_t inbound_data_size = 0;
-        if (!(is >> std::hex >> inbound_data_size)) {
-          // Header doesn't seem to be valid. Inform the caller.
-          boost::system::error_code error(boost::asio::error::invalid_argument);
-          read_handler(ec, T());
-          return;
-        }
-
-        std::cout << "Received header:\n" << inbound_data_size << std::endl;
-
-        // Start an asynchronous call to receive the data.
-        inbound_data_.resize(inbound_data_size);
-        boost::asio::async_read(
-            socket_, boost::asio::buffer(inbound_data_),
-            [&](boost::system::error_code ec, std::size_t length) {
-              if (ec) {
-                read_handler(ec, T());
-                return;
-              }
-
-              T t;
-              try {
-                std::string archive_data(&inbound_data_[0],
-                                         inbound_data_.size());
-                std::istringstream archive_stream(archive_data);
-                boost::archive::text_iarchive archive(archive_stream);
-
-                archive >> t;
-              } catch (std::exception& e) {
-                // Unable to decode data.
-                boost::system::error_code error(
-                    boost::asio::error::invalid_argument);
-                read_handler(ec, T());
-                return;
-              }
-
-              read_handler(ec, t);
-            });
-      });
+std::size_t Connection<T>::parse_header() {
+  std::istringstream is(std::string(inbound_header_, header_length_));
+  std::size_t inbound_data_size = 0;
+  if (!(is >> std::hex >> inbound_data_size)) {
+    throw boost::system::error_code(boost::asio::error::invalid_argument);
+  }
+  return inbound_data_size;
 }
 
 template <typename T>
-void Connection<T>::write(const T& t) {
+T Connection<T>::parse_data() {
+  try {
+    T t;
+    std::string archive_data(&inbound_data_[0], inbound_data_.size());
+    std::istringstream archive_stream(archive_data);
+    boost::archive::text_iarchive archive(archive_stream);
+    archive >> t;
+    return t;
+  } catch (const std::exception& e) {
+    throw boost::system::error_code(boost::asio::error::invalid_argument);
+  }
+}
+
+template <typename T>
+std::string Connection<T>::serialize_data(const T& data) {
   std::ostringstream archive_stream;
   boost::archive::text_oarchive archive(archive_stream);
-  archive << t;
-  outbound_data_ = archive_stream.str();
+  archive << data;
+  return archive_stream.str();
+}
 
+template <typename T>
+std::string Connection<T>::build_header() {
   std::ostringstream header_stream;
   header_stream << std::setw(header_length_) << std::hex
                 << outbound_data_.size();
 
   if (!header_stream || header_stream.str().size() != header_length_) {
-    boost::system::error_code ec(boost::asio::error::invalid_argument);
-    write_handler(ec, 0);
-    return;
+    throw boost::system::error_code(boost::asio::error::invalid_argument);
   }
-  outbound_header_ = header_stream.str();
 
-  // Write the serialized data to the socket. We use "gather-write" to send
-  // both the header and the data in a single write operation.
+  return header_stream.str();
+}
+
+template <typename T>
+void Connection<T>::read() {
+  // read header
+  boost::asio::async_read(
+      socket_, boost::asio::buffer(inbound_header_),
+      [&](boost::system::error_code header_ec, std::size_t header_bytes_read) {
+        if (header_ec) {
+          return read_handler(header_ec, T());
+        }
+
+        try {
+          std::size_t inbound_data_size = parse_header();
+          inbound_data_.resize(inbound_data_size);
+
+          // read message
+          boost::system::error_code message_ec;
+          std::size_t message_bytes_read = boost::asio::read(
+              socket_, boost::asio::buffer(inbound_data_), message_ec);
+          if (message_ec) {
+            return read_handler(message_ec, T());
+          }
+
+          T t = parse_data();
+          read_handler(message_ec, t);
+        } catch (const boost::system::error_code& e) {
+          read_handler(e, T());
+        }
+      });
+}
+
+template <typename T>
+void Connection<T>::write(const T& data) {
+  try {
+    outbound_data_ = serialize_data(data);
+    outbound_header_ = build_header();
+  } catch (const boost::system::error_code& ec) {
+    return write_handler(ec, 0);
+  }
+
   std::vector<boost::asio::const_buffer> buffers;
   buffers.push_back(boost::asio::buffer(outbound_header_));
   buffers.push_back(boost::asio::buffer(outbound_data_));
