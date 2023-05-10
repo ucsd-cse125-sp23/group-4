d87746d0f0f10d69d50f74f6b7a6a39a2f1c7d6f
Adneths <bill.lui0921@gmail.com>
2023-05-09 17:50:36 -0700

    Abstracted vector of PObject to PObjectCollection

---
+++ include/core/game/level/PObjectCollection.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "core/game/physics/PObject.h"
+
+#include <map>
+
+class PObjectCollection {
+private:
+	std::map<size_t, PObject*> objs;
+public:
+	PObjectCollection();
+	void addPObject(PObject* obj);
+	PObject* getById(size_t id);
+	bool removeById(size_t id);
+	std::vector<size_t> getAllIds();
+	void clear();
+
+	PObject* operator[](size_t id);
+};
