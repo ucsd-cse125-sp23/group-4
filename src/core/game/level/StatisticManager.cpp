abbe8781205ed69721e9a5a76cca4cda2babcaa4
Adneths <bill.lui0921@gmail.com>
2023-05-11 02:08:00 -0700

    Implement statistics

--- src/core/game/level/StatisticManager.cpp
+++ src/core/game/level/StatisticManager.cpp
@@ -1,25 +1,23 @@
 #include "core/game/level/StatisticManager.h"
 
 
-template <typename T>
-T StatisticManager::getValue(uint32_t pid, StatisticDefinition<T>* def)
+Stat StatisticManager::getValue(uint32_t pid, StatisticDefinition* def)
 {
 	if (this->playerStatistics[pid].find(def->key) == this->playerStatistics[pid].end())
 	{
-		this->playerStatistics[pid].find[def->key] = def->defaultValue;
+		this->playerStatistics[pid][def->key] = def->defaultValue;
 		return def->defaultValue;
 	}
-	return std::any_cast<decltype(def->defaultValue)>(this->playerStatistics[pid][def->key]);
+	return this->playerStatistics[pid][def->key];
 }
 
 StatisticManager::StatisticManager(const EventManager* eventManager) : eventManager(eventManager)
 {}
 
-template <typename T>
-void StatisticManager::addDefinition(StatisticDefinition<T>* def)
+void StatisticManager::addDefinition(StatisticDefinition* def)
 {
 	this->definitions[def->key] = def;
-	def->registerListeners(evetnManager);
+	def->registerListeners(eventManager);
 }
 
 template <typename T>
