e027db7a2a76cf900530749beb763e2762c98696
Adneths <bill.lui0921@gmail.com>
2023-05-23 05:23:48 -0700

    Tweak player bound radius

--- src/core/util/global.cpp
+++ src/core/util/global.cpp
@@ -14,7 +14,7 @@ int ENVIRONMENT_LAYER = 1;
 int POWER_LAYER = 2;
 
 SphereShape* POWERUP_BASE_SHAPE = new SphereShape(0.4);
-CylinderShape* PLAYER_BASE_SHAPE = new CylinderShape(1.0, 0.5);
+CylinderShape* PLAYER_BASE_SHAPE = new CylinderShape(1.0, 1.3);
 OffsetShape* PLAYER_BOUNDING_SHAPE =
     new OffsetShape(PLAYER_BASE_SHAPE, vec3f(0.0f, 0.5f, 0.0f));
 
