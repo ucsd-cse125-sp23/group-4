7c4d89397b97c8d7e408704af51af073ff188609
Eddie Lin <linhancheng14@gmail.com>
2023-05-28 20:37:43 -0700

    Remove duplicate PlayerModel class draw() method

--- include/client/graphics/PlayerModel.h
+++ include/client/graphics/PlayerModel.h
@@ -41,26 +41,4 @@ class PlayerModel : public Model {
     skel->Update(dt);
     skin->updateSkin(skel);
   }
-
-  virtual void draw(const glm::mat4& viewProjMtx, const glm::mat4& viewMtx,
-                    const glm::mat4& transformMtx,
-                    const bool ignoreDepth = false) {
-    if (!material || !mesh) return;
-
-    GLuint shader = material->shader;
-
-    // actiavte the shader program      ---
-    glUseProgram(shader);
-
-    if (ignoreDepth) glDisable(GL_DEPTH_TEST);
-
-    material->setUniforms(viewProjMtx, viewMtx, transformMtx * modelMtx);
-
-    skin->draw();
-
-    if (ignoreDepth) glEnable(GL_DEPTH_TEST);
-
-    // deactivate the shader program    ---
-    glUseProgram(0);
-  }
 };
