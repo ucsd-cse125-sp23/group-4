54f22d6e1720a711a30fcb6bb858a2f20c80ee9a
Sean Ye <seanye24@gmail.com>
2023-06-01 11:39:34 -0700

    Basic lobby implementation (#52)
    * Rename create_player to add_player
    
    * Refactor GameThing
    
    * Make static first
    
    * Reformat
    
    * Add lobby messages
    
    * Add basic Manager interface
    
    * Separate and implement Manager
    
    * Replace game with manager
    
    * Add game start message
    
    * Change server tick to be manually triggered
    
    * Add basic lobby implementation
    
    * Fix header formatting
    
    * Rename class members
    
    * Remove desync (unnecessary)
    
    * Initialize variable
    
    ---------
    
    Co-authored-by: Sean Ye <seanye24@gmai.com>

---
+++ include/server/manager.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <network/message.hpp>
+#include <server/game.hpp>
+#include <string>
+#include <unordered_map>
+
+class Manager {
+ public:
+  enum class Status {
+    Lobby,
+    InGame,
+    GameOver,
+  };
+
+  static const std::size_t MAX_PLAYERS = 2;
+
+  int add_player();
+  void remove_player(int);
+  message::LobbyUpdate handle_lobby_update(const message::LobbyPlayerUpdate&);
+  message::LobbyUpdate get_lobby_update();
+  bool check_ready();
+  void handle_game_update(const message::UserStateUpdate&);
+  void tick_game();
+  message::GameStateUpdate get_game_update();
+
+  Status status_ = Status::Lobby;
+
+ private:
+  struct Player {
+    int pid;
+    std::string skin;
+    bool is_ready;
+  };
+
+  Game game_;
+  std::unordered_map<int, Player> players_;
+};
