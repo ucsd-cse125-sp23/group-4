6e814e0dbc4c02e142e9a6f21671bc7a20a2bbca
Hancheng Lin <linhancheng14@gmail.com>
2023-06-01 00:53:51 -0700

    Fix parallel utility class clang-format error

--- include/client/graphics/imported/parallel.h
+++ include/client/graphics/imported/parallel.h
@@ -1,17 +1,18 @@
 #pragma once
 
-//#include "vector.h"
+// #include "vector.h"
 
-#include <mutex>
-#include <functional>
 #include <atomic>
+#include <functional>
+#include <mutex>
 
 // CSE168 Utility Class
 // From https://github.com/mmp/pbrt-v3/blob/master/src/core/parallel.h
 extern thread_local int ThreadIndex;
 
-void parallel_for(const std::function<void(int)> &func, int64_t count, int64_t chunk_size = 1);
-//void parallel_for(std::function<void(Vector2i)> func, const Vector2i count);
+void parallel_for(const std::function<void(int)> &func, int64_t count,
+                  int64_t chunk_size = 1);
+// void parallel_for(std::function<void(Vector2i)> func, const Vector2i count);
 
 void parallel_init(int num_threads);
 void parallel_cleanup();
