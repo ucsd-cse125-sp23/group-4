9c2b870822faf1bfa953e30af26278f2d28735ec
Sean Ye <seanye24@gmail.com>
2023-06-08 16:43:19 -0700

    Make item public

--- include/core/game/physics/PowerUp.h
+++ include/core/game/physics/PowerUp.h
@@ -1,20 +1,21 @@
 #pragma once
 
+#include <chrono>
+#include <random>
+
 #include "core/game/effect/GlobalEffect.h"
 #include "core/game/physics/PObject.h"
 #include "network/item.hpp"
-#include <random>
-#include <chrono>
-
 
 class PowerUp : public PObject {
  private:
   static std::mt19937 rng;
 
   GlobalEffect* effect;
-  Item item;
 
  public:
   PowerUp(vec3f pos, GlobalEffect* effect);
   void onTrigger(PObject* other) override;
+
+  Item item;
 };
